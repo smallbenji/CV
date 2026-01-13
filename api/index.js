c042d10ab7140d73db809d2a541c907efe305ced
smallbenji <benjaminkfalch@gmail.com>
2026-01-13 02:12:57 +0100

    adding backend API

---
+++ api/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const PROM_URL = "http://ssh.smallbenji.tech:9090";
+const app = express();
+
+app.use(cors());
+
+app.get("/status", async (req, res) => {
+    try {
+        const query = "probe_success{job=\"blackbox\"}";
+        const url = `${PROM_URL}/api/v1/query?query=${encodeURIComponent(query)}`;
+
+        const r = await fetch(url, {timeout: 5000});
+        if (!r.ok) throw new Error("Prometheus error");
+
+        const data = await r.json();
+
+        const result = [];
+        for (const item of data.data.result) {
+            const target = item.metric.instance || "unknown";
+            result.push({
+                site: target,
+                up: item.value[1] === "1" ? true : false
+            });
+        }
+
+        res.json(result);
+    } catch (err) {
+        res.status(502).json({error: "Prometheus unavailable"});
+    }
+});
+
+app.listen(3000, () => {
+    console.log("API listening on port 3000");
+})
